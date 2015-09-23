7c914f6a9d7e63bb031978edea8085d5e1d82d21
ZhengLeiGao <15620629809@sina.cn>
2015-09-23 17:50:08 +0800

    addActivityListTableViewCell
    addActivityListTableViewCell

---
+++ Party/Views/ActivityViews/ActivityListCell/ActivityListTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ActivityListTableViewCell.h
+//  Party
+//
+//  Created by qianfeng on 15/9/23.
+//  Copyright (c) 2015年 高正磊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ActivityListTableViewCell : UITableViewCell
+
+@end
---
+++ Party/Views/ActivityViews/ActivityListCell/ActivityListTableViewCell.m
@@ -0,0 +1,30 @@
+//
+//  ActivityListTableViewCell.m
+//  Party
+//
+//  Created by qianfeng on 15/9/23.
+//  Copyright (c) 2015年 高正磊. All rights reserved.
+//
+
+#import "ActivityListTableViewCell.h"
+
+@implementation ActivityListTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    
+    
+    
+
+    /**
+     *  <#Description#>
+     */
+    // Configure the view for the selected state
+}
+
+@end
